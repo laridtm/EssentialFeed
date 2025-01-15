2a34077150e5417ceec5391b8907944d2714aa96
Larissa Diniz Martins <larissa.diniz@ifood.com.br>
2025-01-15 11:24:21 -0300

    Feed Cache integration tests

---
+++ EssentialFeedCacheIntegrationTests/EssentialFeedCacheIntegrationTests.swift
@@ -0,0 +1,16 @@
+//
+//  EssentialFeedCacheIntegrationTests.swift
+//  EssentialFeedCacheIntegrationTests
+//
+//  Created by Larissa Diniz on 15/01/25.
+//
+
+import Testing
+
+struct EssentialFeedCacheIntegrationTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
